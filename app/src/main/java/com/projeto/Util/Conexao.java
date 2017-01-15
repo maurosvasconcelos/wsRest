14a6afd7f15efc30f729ec9d0d9e594903d60017
maurosvasconcelos <mauro.analise@hotmail.com>
2017-01-15 12:28:14 -0200

    teste

---
+++ app/src/main/java/com/projeto/Util/Conexao.java
@@ -0,0 +1,16 @@
+package com.projeto.Util;
+
+/**
+ * Created by mauro on 15/01/2017.
+ */
+
+public class Conexao {
+    public static String IP_CASA_URL = "http://192.168.1.5:8084/RestFulWS/pessoa/";
+    public static String IP_TRB_URL = "http://192.168.82.126:8084/RestFulWS/pessoa/";
+    public static String IP_WEB_URL = "http://189.112.226.70:85/RestFulWS/pessoa/";
+    public static String INCLUIR = "incluir";
+    public static String LISTAR_TODOS = "listarTodos";
+    public static String CONSULTAR_CPF = "consultarCpf";
+
+
+}
